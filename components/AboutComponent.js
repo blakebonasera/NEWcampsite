e2f31cd55d613185d91c8d1cf6070b0f01f8654e
Blake <blakebonasera@gmail.com>
2020-04-29 12:57:29 -0700

    Using Redux

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
-import {ScrollView, Text, FlatList} from 'react-native';
+import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
-import {PARTNERS} from '../shared/partners';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+      partners: state.partners
+    };
+};
+
 
 function Mission(){
     return(
@@ -36,7 +44,7 @@ class About extends Component {
                 <ListItem
                 title= {item.name}
                 subtitle={item.description}
-                leftAvatar={{source:require('./images/bootstrap-logo.png')}}
+                leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
             );
         }
@@ -47,11 +55,11 @@ class About extends Component {
                 <Card 
                     title="Community Partners"
                 >
-                <FlatList  
-                    data ={this.state.partners}
+                <FlatList 
+                    data={this.props.partners.partners}
                     renderItem={renderPartner}
                     keyExtractor={item => item.id.toString()}
-                />
+                    />
                 </Card>
             </ScrollView>
             
@@ -59,4 +67,4 @@ class About extends Component {
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
-import { COMMENTS } from '../shared/comments';
-import { CAMPSITES } from '../shared/campsites';
+import { connect } from 'react-redux';
+import { baseUrl } from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        campsites: state.campsites,
+        comments: state.comments
+    };
+};
+
 
 function RenderCampsite(props) {
 
@@ -12,7 +20,7 @@ function RenderCampsite(props) {
         return (
         <Card
             featuredTitle={campsite.name}
-            image={require('./images/react-lake.jpg')}>
+            image={{uri: baseUrl + campsite.image}}>
             <Text style={{margin: 10}}>
                 {campsite.description}
             </Text>
@@ -90,4 +98,4 @@ class CampsiteInfo extends Component {
     }
 }
 
-export default CampsiteInfo;
+export default connect(mapStateToProps)(CampsiteInfo);
